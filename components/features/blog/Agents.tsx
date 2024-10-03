b9d94fa113aafda84d2db98b0d56b70c44faf369
Kofi Gyimah <godsgeneral625@gmail.com>
2024-10-03 07:58:19 +0000

    chore: 👨‍🔧 minor changes

--- components/features/blog/Agents.tsx
+++ components/features/blog/Agents.tsx
@@ -4,39 +4,39 @@ import { setImage } from '@/utils';
 import Image from 'next/image';
 import Rating from './Rating';
 
-export const AgentsCard = ({ user, total_rating, type }: Agent) => {
+export const AgentsCard = ({ user, total_rating, type, host_user_name }: Agent) => {
   return (
     <div className="border rounded-3xl p-3 mb-5">
-      <div className="grid grid-cols-4 gap-3">
-        <div className="col-span-1">
-          <div className="h-[5rem] w-[5rem] rounded-full overflow-hidden relative">
-            <img
-              src={setImage(user?.firstname, user?.lastname, user?.profileImage)}
-              alt={user?.firstname}
-              className="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-
-        <div className="col-span-3">
-          <div className="flex justify-between">
-            <div>
-              <h3>
-                {user.firstname} {user.lastname}
-              </h3>
-              <p className="text-[#D62151] text-[12px]">{type.name}</p>
-              <div className="flex gap-2 items-baseline">
-                <Rating rating={total_rating} />
-                <span>{total_rating > 1 && total_rating}</span>
-              </div>
+      <a href={`/host/${host_user_name}`}>
+        <div className="grid grid-cols-4 gap-3">
+          <div className="col-span-1">
+            <div className="h-[5rem] w-[5rem] rounded-full overflow-hidden relative">
+              <img
+                src={setImage(user?.firstname, user?.lastname, user?.profileImage)}
+                alt={user?.firstname}
+                className="w-full h-full object-cover"
+              />
             </div>
+          </div>
 
-            <a href={`tel:${user.phone}`}>
+          <div className="col-span-3">
+            <div className="flex justify-between">
+              <div>
+                <h3>
+                  {user.firstname} {user.lastname}
+                </h3>
+                <p className="text-[#D62151] text-[12px]">{type.name}</p>
+                <div className="flex gap-2 items-baseline">
+                  <Rating rating={total_rating} />
+                  <span>{total_rating > 1 && total_rating}</span>
+                </div>
+              </div>
+
               <Image src="/blog/call.svg" alt="Logo" width={50} height={50} />
-            </a>
+            </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
